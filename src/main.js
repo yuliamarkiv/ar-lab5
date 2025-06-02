7d153541bf2bd36d55164f79c1e9e4d79fb80867
nazarmac <test@gmail.com>
2025-06-02 23:44:04 +0200

    Test automatic deploy

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 import "./style.css";
 
 document.getElementById("task1Btn").addEventListener("click", () => {
-  window.location.href = "src/task1.html";
+  window.location.href = "/src/task1.html";
 });
 
 document.getElementById("task2Btn").addEventListener("click", () => {
-  window.location.href = "src/task2.html";
+  window.location.href = "/src/task2.html";
 });
 
 document.getElementById("task3Btn").addEventListener("click", () => {
-  window.location.href = "src/task3.html";
+  window.location.href = "/src/task3.html";
 });
 
 document.getElementById("task4Btn").addEventListener("click", () => {
-  window.location.href = "src/task4.html";
+  window.location.href = "/src/task4.html";
 });
