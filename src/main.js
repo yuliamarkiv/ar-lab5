eafde7b7b1c3c655d08ea22678f38ca8c19a0efa
nazarmac <test@gmail.com>
2025-06-02 23:48:56 +0200

    Test automatic deploy

--- src/main.js
+++ src/main.js
@@ -1,17 +1,40 @@
 import "./style.css";
 
+function getBaseUrl() {
+    // Get the repository name from the pathname (will be empty locally)
+    const pathParts = window.location.pathname.split('/');
+    let repoName = '';
+
+    // Check if we're on GitHub Pages
+    if (window.location.hostname.includes('github.io')) {
+        // The repository name is the first path segment after the domain on GitHub Pages
+        repoName = pathParts[1];
+        return window.location.origin + '/' + repoName + '/';
+    } else {
+        // Local development
+        return window.location.origin + '/';
+    }
+}
+
+const baseUrl = getBaseUrl();
+console.log('haha url', baseUrl);
+
 document.getElementById("task1Btn").addEventListener("click", () => {
-  window.location.href = "/src/task1.html";
+    window.location.href = baseUrl
+        + "src/task1.html";
 });
 
 document.getElementById("task2Btn").addEventListener("click", () => {
-  window.location.href = "/src/task2.html";
+    window.location.href = baseUrl
+        + "/src/task2.html";
 });
 
 document.getElementById("task3Btn").addEventListener("click", () => {
-  window.location.href = "/src/task3.html";
+    window.location.href = baseUrl
+        + "/src/task3.html";
 });
 
 document.getElementById("task4Btn").addEventListener("click", () => {
-  window.location.href = "/src/task4.html";
+    window.location.href = baseUrl
+        + "/src/task4.html";ç
 });
